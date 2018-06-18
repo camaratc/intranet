9a715f6a51eec6d660deae9ba03f3d10814cf431
Matheus R <matheusrf96@gmail.com>
2018-06-18 13:58:34 -0300

    Alteração header

--- wp-content/themes/patus/header.php
+++ wp-content/themes/patus/header.php
@@ -9,11 +9,12 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -22,6 +23,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container">
+
 			<div class="site-branding">
 				<?php
                 if ( function_exists( 'has_custom_logo' ) ) {
@@ -30,12 +32,26 @@
                     }
                 }
                 ?>
-                <?php if ( is_front_page() && is_home() ) { ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                <?php } else {  ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-                <?php } ?>
-                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+                <div class="parent">
+	                <div class="children">
+	                	<div class="child">
+			                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png"
+			                	height="100px"
+			                	width="75px;"
+			                />
+			             </div>
+	            	</div>
+	            	<div class="children">
+	            		<div class="child">
+			                <?php if ( is_front_page() && is_home() ) { ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			                <?php } else {  ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			                <?php } ?>
+			                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			            </div>
+		            </div>
+	        	</div>
 			</div>
 		</div>
 
